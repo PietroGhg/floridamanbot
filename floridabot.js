0d10419818cf413db2000769e44c2e9d1ef0dfc7
Sav <56634996+Sav-bit@users.noreply.github.com>
2020-02-15 18:56:17 +0100

    Now it can read any layout page

--- floridabot.js
+++ floridabot.js
@@ -8,68 +8,53 @@ const monthNames = ["january", "february", "march", "april", "may", "june",
 ];
 
 async function scrapeFloridaMan(url){
+  console.log("started : "+url);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
-    let [el] = await page.$x('/html/body/div[1]/main/div/div/div[1]/article/div/div[1]/h2');
-    if(el != null){
-	//Layout 1
-	let txt = await el.getProperty('textContent');
-
-	let rawTxt = await txt.jsonValue();
-
-	console.log(rawTxt);
-
-	[el] = await page.$x('//*[@class="wp-block-image"]/a/img');///html/body/div[1]/main/div/div/div[1]/article/div/header/div[2]/a/img');
-	let img = await el.getProperty('src');
-	let rawImg = await img.jsonValue();
-	var viewSource = await page.goto(rawImg);
-	fs.writeFile("./florida.jpg", await viewSource.buffer(),
-		     function(err) {
-			 if(err) { return console.log(err); }
-			 console.log("The file was saved!"); });
-
-	return rawTxt;
-    }
-    else{
-	//Layout 2
-	[el] = await page.$x('/html/body/div[1]/main/div/div/div[1]/article/div/div[1]/p[2]');
-	if(el != null){
-	    let txt = await el.getProperty('textContent');
-
-	    let rawTxt = await txt.jsonValue();
-	    
-	    [el] = await page.$x('//*[@class="wp-block-image"]/img');///html/body/div[1]/main/div/div/div[1]/article/div/div[1]/figure/img');
-	    let img = await el.getProperty('src');
-	    let rawImg = await img.jsonValue();
-	    var viewSource = await page.goto(rawImg);
-	    fs.writeFile("./florida.jpg", await viewSource.buffer(),
-			 function(err) {
-			     if(err) { return console.log(err); }
-			     console.log("The file was saved!"); });
-	    return rawTxt;
-	}
-	else{
-	    //Layout 3
-	    [el] = await page.$x('/html/body/div[1]/main/div/div/div[1]/article/div/div[1]/p[2]');
-	    let txt = await el.getProperty('textContent');
-
-	    let rawTxt = await txt.jsonValue();
-	    
-	    [el] = await page.$x('//*[@class="wp-block-image"]/img');///html/body/div[1]/main/div/div/div[1]/article/div/header/div[2]/a/img');	                          
-	    let img = await el.getProperty('src');
-	    let rawImg = await img.jsonValue();
-	    var viewSource = await page.goto(rawImg);
-	    fs.writeFile("./florida.jpg", await viewSource.buffer(),
-			 function(err) {
-			     if(err) { return console.log(err); }
-			     console.log("The file was saved!"); });
-
-	    return rawTxt;
-	}
+  await page.goto(url,{timeout: 0});
+  let titlePage = await page.title();
+  if(!(titlePage.toLowerCase().includes('page not found'))){
+    let img2
+    //method 2 (more elegant):
+    try {
+      //check for img inside a parent with class entry-image
+      img2 = await page.$eval('.entry-image img', img => img.src);
+      console.log(img2);
+      //if there are none it's because the image is in the content and not the header
+    } catch (e) {
+      //check for images inside the entry content
+      img2 = await page.$eval('.entry-content img', img => img.src);
+      console.log(img2);
     }
 
+    let viewSource = await page.goto(img2);
+    fs.writeFileSync("./florida.jpg", await viewSource.buffer(), function(err) { if(err) { return console.log(err); } console.log("The file was saved!"); });
+    let title;
+    page.goBack({timeout: 0});
+    try{
+      console.log("in try");
+      //check for header in the entry content
+      let h2 = await page.$eval('.entry-content > h2', h2 => h2.textContent);
+      console.log(h2);
+      console.log("Done for "+url);
+      return h2;
+      //if there are none, throws an error
+    }catch(e){
+      //catch the error then find the text in the paragraph
+      console.log("in catch");
+      let title = await page.$$eval('.entry-content > p', p => p.map(img => img.textContent));
+      title.splice(0,1);
+      title.pop();
+      console.log(title);
+      return title.join('\n');
+    }
+  }
+  else{
+    console.log("404");
+    return null;
+  }
 }
+
 
 function randomDate(start, end) {
   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
@@ -102,8 +87,15 @@ bot.command('news', async (message) => {
     let news = await scrapeFloridaMan('http://thefloridamantimes.com/' + today + '/');
 
 
-    bot.telegram.sendMessage(message.chat.id, news);
-    bot.telegram.sendPhoto(message.chat.id, {source: './florida.jpg'})
+    if(news == null){
+      bot.telegram.sendMessage(message.chat.id,"Surprisingly, the florida man did nothing crazy that day, maybe he rested ... \n "+
+      "why don't you do the same? No one runs after you, all the problems you think you have are temporary, take a moment for yourself.\n"+
+      "Here is a video by bob ross to remind you that life is beautiful : https://youtu.be/nsXuGvfMj64");
+    }
+    else{
+      bot.telegram.sendMessage(message.chat.id, news);
+      bot.telegram.sendPhoto(message.chat.id, {source: './florida.jpg'})
+    }
 });
 
 
@@ -117,16 +109,22 @@ bot.command('whosabitch', (message) => {
 bot.command('random', async message =>{
 
   let random = randomDate(new Date(2012, 0, 1), new Date());
-    let dd = parseInt(String(random.getDate()).padStart(2, '0'));
+  let dd = parseInt(String(random.getDate()).padStart(2, '0'));
   let mm = String(random.getMonth()).padStart(2, '0'); //January is 0!
 
   random = monthNames[parseInt(mm)] + '-' + dd;
   console.log(random);
   let news = await scrapeFloridaMan('http://thefloridamantimes.com/' + random + '/');
 
-  bot.telegram.sendMessage(message.chat.id, news);
-  bot.telegram.sendPhoto(message.chat.id, {source: './florida.jpg'})
-
+  if(news == null){
+    bot.telegram.sendMessage(message.chat.id,"Surprisingly, the florida man did nothing crazy that day, maybe he rested ... \n "+
+    "why don't you do the same? No one runs after you, all the problems you think you have are temporary, take a moment for yourself.\n"+
+    "Here is a video by bob ross to remind you that life is beautiful : https://youtu.be/nsXuGvfMj64");
+  }
+  else{
+    bot.telegram.sendMessage(message.chat.id, news);
+    bot.telegram.sendPhoto(message.chat.id, {source: './florida.jpg'})
+  }
   console.log(random);
 
 });
